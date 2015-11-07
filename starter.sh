ca15fe5416fa002bed6c87026932f54bd301dbfc
Jeff Greer <greergan@gmail.com>
2015-11-07 12:18:24 -0800

    removed startup script as it should not be managed in source control.

--- starter.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ $(ps -e -o uid,cmd | grep $UID | grep node | grep test | grep -v grep | wc -l | tr -s "\n") -eq 0 ]
-then
-        export PATH=/usr/local/bin:$PATH
-        cd /home/www/test
-        pbctrl start forever  >> startlog.txt 2>&1
-fi
