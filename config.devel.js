16372fa5d617ad37ee2b4ba5830dc27107b74921
Angus Woodbury <atacra@gmail.com>
2016-01-11 23:35:48 -0700

    Rename default config files

--- config.devel.js
+++ config.devel.js
@@ -10,7 +10,7 @@
  */
 
 module.exports = {
-    "siteName": "My PencilBlue Site",
+    "siteName": "Startup Satna Fe",
     "siteRoot": "http://127.0.0.1:8080",
     "sitePort": 8080,
     "logging": {
@@ -21,8 +21,31 @@ module.exports = {
         "servers": [
           "127.0.0.1:27017"
         ],
-        "name": "ssf_devel",
-        "writeConcern": 1
+        "name": "pencil_blue",
+        "writeConcern": 1,
+			
+        //This option instructs the child to skip the checks to ensure that the
+        //indices are built.  It makes the assumption that the user doesn't care
+        //or that they are already in place.  This would typically be used in a
+        //large production system where load can burst.  In that particular case
+        //you wouldn't want to let your instances annoy the DB to check for
+        //indices because it would cause greater strain on the DB under heavy
+        //load.
+        skip_index_check: false,
+
+        //The indices that will be ensured by the system.  This list is checked
+        //at startup by every child process.  The override config.json file may
+        //also provide this attribute.  In that case the items in that array
+        //will be added to the those that already exist.  NOTE: duplicates can
+        //exist.
+        indices: [
+            //custom objects
+            {
+                collection: 'custom_object',
+                spec: {resource_group: ASC},
+                options: { }
+            }
+				]
     },
     "cache": {
         "fake": true,
@@ -56,5 +79,12 @@ module.exports = {
     "cluster": {
         "workers": 1,
         "self_managed": true
+    },
+
+    "logging": {
+        
+        level: "debug",
+        file: LOG_FILE,
+        showErrors: true
     }
 };
