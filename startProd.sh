a478dd6f6f1a8340d5854bd7be09c3f644c0762a
Angus Woodbury <atacra@gmail.com>
2016-02-18 15:34:29 -0700

    Fixed syntax error in start production script

--- startProd.sh
+++ startProd.sh
@@ -2,12 +2,12 @@
 
 if [ $(ps -e -o uid,cmd | grep $UID | grep node | grep prod | wc -l | tr -s "\n") -eq 0 ]
 then
-	echo “Prod Server: starting"
+	echo "Prod Server: starting"
 	export PATH=/usr/local/bin:$PATH
 	cd /home/www/prod
 	pbctrl start forever >> startlog.txt 2>&1
 else
-	echo “Prod Server: restarting"
+	echo "Prod Server: restarting"
 	cd /home/www/prod
 	forever restart /home/www/prod/pencilblue.js >> startlog.txt 2>&1
 fi
